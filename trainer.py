1103421c87835de5084a0d265d0ec0a142825120
fogyshion <fogyshion@163.com>
2025-07-05 17:01:46 +0800

    针对inception_v3，重写前向与反向传播代码

--- trainer.py
+++ trainer.py
@@ -28,3 +28,29 @@ class Trainer(basic.Trainer):
     def get_loss_criterion(self):
         """Returns the loss criterion."""
         return self.process_loss
+
+    def perform_forward_and_backward_passes(self, config, examples, labels):
+        """
+        重载父类方法，针对inception_v3修改
+        """
+        self.optimizer.zero_grad()
+
+        outputs = self.model(examples)
+
+        # 原始代码
+        # loss = self._loss_criterion(outputs, labels)
+
+        # 修改代码（针对inception_v3）
+        logits = outputs.logits
+        loss = self._loss_criterion(logits, labels)
+
+        self._loss_tracker.update(loss, labels.size(0))
+
+        if "create_graph" in config:
+            loss.backward(create_graph=config["create_graph"])
+        else:
+            loss.backward()
+
+        self.optimizer.step()
+
+        return loss
